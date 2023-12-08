f034060d23afa5835a5ac0290b5efa9a6458f632
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-12-08 23:15:27 +0100

    translated property names from french to english

--- avis.js
+++ avis.js
@@ -11,9 +11,9 @@ export function reviewsAddListener() {
 
       const reviewsElement = document.createElement('p');
       for (let i = 0; i < reviews.length; i++) {
-        reviewsElement.innerHTML += `${reviews[i].utilisateur}: ${reviews[i].commentaire} <br>`;
+        reviewsElement.innerHTML += `${reviews[i].user}: ${reviews[i].comment} <br>`;
       }
-      pieceElement.appendChild(avisElement);
+      pieceElement.appendChild(reviewsElement);
     });
   }
 }
--- pieces.js
+++ pieces.js
@@ -2,7 +2,7 @@ import { reviewsAddListener } from './avis.js';
 
 // Retrieve parts from the JSON file
 // const pieces = await fetch('pieces-autos.json').then(pieces => pieces.json());
-const response = await fetch('pieces-autos.json');
+const response = await fetch('http://localhost:8081/pieces');
 const pieces = await response.json();
 
 // Function that generates the entire web page
