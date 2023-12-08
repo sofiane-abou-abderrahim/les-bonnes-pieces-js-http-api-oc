48e87119b2f0d49c7a52c1dd8855b8f379fed690
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-12-08 22:56:23 +0100

    saved server response in a constant + used await/async

--- avis.js
+++ avis.js
@@ -2,9 +2,9 @@ export function reviewsAddListener() {
   const piecesElements = document.querySelectorAll('.sheets article button');
 
   for (let i = 0; i < piecesElements.length; i++) {
-    piecesElements[i].addEventListener('click', function (event) {
+    piecesElements[i].addEventListener('click', async function (event) {
       const id = event.target.dataset.id;
-      fetch(`http://localhost:8081/pieces/${id}/avis`);
+      const response = await fetch(`http://localhost:8081/pieces/${id}/avis`);
     });
   }
 }
