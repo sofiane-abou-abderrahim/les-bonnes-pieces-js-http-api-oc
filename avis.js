65b95573831d24b116df905c4967f7c840aef3ff
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-12-09 14:48:49 +0100

    fixed all errors with eslint

--- avis.js
+++ avis.js
@@ -1,149 +1,147 @@
+/* global Chart */
 export function reviewsAddListener() {
-  const piecesElements = document.querySelectorAll('.sheets article button');
+	const piecesElements = document.querySelectorAll('.sheets article button');
 
-  for (let i = 0; i < piecesElements.length; i++) {
-    piecesElements[i].addEventListener('click', async function (event) {
-      const id = event.target.dataset.id;
-      const response = await fetch(`http://localhost:8081/pieces/${id}/avis`);
-      const reviews = await response.json();
+	for (let i = 0; i < piecesElements.length; i++) {
+		piecesElements[i].addEventListener('click', async function (event) {
+			const id = event.target.dataset.id;
+			const response = await fetch(`http://localhost:8081/pieces/${id}/avis`);
+			const reviews = await response.json();
 
-      // Call setItem
-      window.localStorage.setItem(
-        `piece-reviews-${id}`,
-        JSON.stringify(reviews)
-      );
+			// Call setItem
+			window.localStorage.setItem(
+				`piece-reviews-${id}`,
+				JSON.stringify(reviews)
+			);
 
-      const pieceElement = event.target.parentElement;
+			const pieceElement = event.target.parentElement;
 
-      // Call the function
-      displayReviews(pieceElement, reviews);
-    });
-  }
+			// Call the function
+			displayReviews(pieceElement, reviews);
+		});
+	}
 }
 
 // Created function to display reviews
 export function displayReviews(pieceElement, reviews) {
-  const reviewsElement = document.createElement('p');
-  for (let i = 0; i < reviews.length; i++) {
-    reviewsElement.innerHTML += `${reviews[i].user}: ${reviews[i].comment} <br>`;
-    reviewsElement.innerHTML += `${reviews[i].nbStars} <br>`;
-  }
-  pieceElement.appendChild(reviewsElement);
+	const reviewsElement = document.createElement('p');
+	for (let i = 0; i < reviews.length; i++) {
+		reviewsElement.innerHTML += `${reviews[i].user}: ${reviews[i].comment} <br>`;
+		reviewsElement.innerHTML += `${reviews[i].nbStars} <br>`;
+	}
+	pieceElement.appendChild(reviewsElement);
 }
 
 export function sendReviewsAddListener() {
-  const reviewsForm = document.querySelector('.reviews-form');
-  reviewsForm.addEventListener('submit', function (event) {
-    // Disable the default behavior of the browser
-    event.preventDefault();
+	const reviewsForm = document.querySelector('.reviews-form');
+	reviewsForm.addEventListener('submit', function (event) {
+		// Disable the default behavior of the browser
+		event.preventDefault();
 
-    // Creating the object for the new reviews
-    const reviews = {
-      pieceId: parseInt(event.target.querySelector('[name=piece-id]').value),
-      user: event.target.querySelector('[name=user').value,
-      comment: event.target.querySelector('[name=comment]').value,
-      nbStars: parseInt(event.target.querySelector('[name=nbStars]').value)
-    };
+		// Creating the object for the new reviews
+		const reviews = {
+			pieceId: parseInt(event.target.querySelector('[name=piece-id]').value),
+			user: event.target.querySelector('[name=user').value,
+			comment: event.target.querySelector('[name=comment]').value,
+			nbStars: parseInt(event.target.querySelector('[name=nbStars]').value)
+		};
 
-    // Create the payload in a JSON format
-    const payload = JSON.stringify(reviews);
+		// Create the payload in a JSON format
+		const payload = JSON.stringify(reviews);
 
-    // Call the fetch function with all the necessary information
-    fetch('http://localhost:8081/avis', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: payload
-    });
-  });
+		// Call the fetch function with all the necessary information
+		fetch('http://localhost:8081/avis', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: payload
+		});
+	});
 }
 
 // Chart.js display reviews graphic
 export async function displayReviewsGraphic() {
-  // Calculate the total number of comments per quantity of stars awarded
-  const reviews = await fetch('http://localhost:8081/avis').then(reviews =>
-    reviews.json()
-  );
-  // Create an array of 5 elements initialized to 0
-  const nb_comments = [0, 0, 0, 0, 0];
+	// Calculate the total number of comments per quantity of stars awarded
+	const reviews = await fetch('http://localhost:8081/avis').then(reviews =>
+		reviews.json()
+	);
+	// Create an array of 5 elements initialized to 0
+	const nb_comments = [0, 0, 0, 0, 0];
 
-  // Traverse through the reviews
-  for (let comment of reviews) {
-    // Increment the elements in the list corresponding to the number of stars awarded
-    nb_comments[comment.nbStars - 1]++;
-  }
+	// Traverse through the reviews
+	for (let comment of reviews) {
+		// Increment the elements in the list corresponding to the number of stars awarded
+		nb_comments[comment.nbStars - 1]++;
+	}
 
-  ///// Prepare the configuration for the chart /////
-  // Legend that will be displayed on the left next to the horizontal bar
-  const labels = ['5', '4', '3', '2', '1'];
-  // Data and customization of the chart
-  const data = {
-    labels: labels,
-    datasets: [
-      {
-        label: 'Étoiles attribuées',
-        data: nb_comments.reverse(),
-        backgroundColor: 'rgba(255, 230, 0, 1)' // Yellow color
-      }
-    ]
-  };
-  // Final configuration object
-  const config = {
-    type: 'bar',
-    data: data,
-    options: {
-      indexAxis: 'y'
-    }
-  };
-  // Render the chart in the canvas element
-  const reviewsGraphic = new Chart(
-    document.querySelector('#reviews-graphic'),
-    config
-  );
+	///// Prepare the configuration for the chart /////
+	// Legend that will be displayed on the left next to the horizontal bar
+	const labels = ['5', '4', '3', '2', '1'];
+	// Data and customization of the chart
+	const data = {
+		labels: labels,
+		datasets: [
+			{
+				label: 'Étoiles attribuées',
+				data: nb_comments.reverse(),
+				backgroundColor: 'rgba(255, 230, 0, 1)' // Yellow color
+			}
+		]
+	};
+	// Final configuration object
+	const config = {
+		type: 'bar',
+		data: data,
+		options: {
+			indexAxis: 'y'
+		}
+	};
+	// Render the chart in the canvas element
+	new Chart(document.querySelector('#reviews-graphic'), config);
 
-  // Retrieve pieces from localStorage
-  const piecesJSON = window.localStorage.getItem('pieces');
-  const pieces = JSON.parse(piecesJSON);
+	// Retrieve pieces from localStorage
+	const piecesJSON = window.localStorage.getItem('pieces');
+	const pieces = JSON.parse(piecesJSON);
 
-  // Calculate the number of comments
-  let nbAvailableComments = 0;
-  let nbUnvailableComments = 0;
+	// Calculate the number of comments
+	let nbAvailableComments = 0;
+	let nbUnvailableComments = 0;
 
-  // Iterate through the list of pieces
-  for (let i = 0; i < reviews.length; i++) {
-    const piece = pieces.find(p => p.id === reviews[i].pieceId);
+	// Iterate through the list of pieces
+	for (let i = 0; i < reviews.length; i++) {
+		const piece = pieces.find(p => p.id === reviews[i].pieceId);
 
-    // Increment the correct variable using an if-else condition
-    if (piece) {
-      if (piece.availability) {
-        nbAvailableComments++;
-      } else {
-        nbUnvailableComments++;
-      }
-    }
-  }
+		// Increment the correct variable using an if-else condition
+		if (piece) {
+			if (piece.availability) {
+				nbAvailableComments++;
+			} else {
+				nbUnvailableComments++;
+			}
+		}
+	}
 
-  // Legend that will be displayed on the left next to the horizontal bar
-  const AvailableLabels = ['Disponibles', 'Non dispo.'];
+	// Legend that will be displayed on the left next to the horizontal bar
+	const AvailableLabels = ['Disponibles', 'Non dispo.'];
 
-  // Data and customization of the chart
-  const AvailableData = {
-    labels: AvailableLabels,
-    datasets: [
-      {
-        label: 'Nombre de commentaires',
-        data: [nbAvailableComments, nbUnvailableComments],
-        backgroundColor: 'rgba(0, 230, 255, 1)' // Turquoise color
-      }
-    ]
-  };
+	// Data and customization of the chart
+	const AvailableData = {
+		labels: AvailableLabels,
+		datasets: [
+			{
+				label: 'Nombre de commentaires',
+				data: [nbAvailableComments, nbUnvailableComments],
+				backgroundColor: 'rgba(0, 230, 255, 1)' // Turquoise color
+			}
+		]
+	};
 
-  // Final configuration object
-  const AvailableConfig = {
-    type: 'bar',
-    data: AvailableData
-  };
-  console.log(AvailableData);
+	// Final configuration object
+	const AvailableConfig = {
+		type: 'bar',
+		data: AvailableData
+	};
+	console.log(AvailableData);
 
-  // Rendu du graphique dans l'élément canvas
-  new Chart(document.querySelector('#availables-graphic'), AvailableConfig);
+	// Rendu du graphique dans l'élément canvas
+	new Chart(document.querySelector('#availables-graphic'), AvailableConfig);
 }
--- pieces.js
+++ pieces.js
@@ -1,25 +1,25 @@
 import {
-  reviewsAddListener,
-  sendReviewsAddListener,
-  displayReviews,
-  displayReviewsGraphic
+	reviewsAddListener,
+	sendReviewsAddListener,
+	displayReviews,
+	displayReviewsGraphic
 } from './avis.js';
 
 // Retrieving pieces potentially stored in localStorage
 let pieces = window.localStorage.getItem('pieces');
 if (pieces === null) {
-  // Retrieve parts from the JSON file
-  // const pieces = await fetch("http://localhost:8081/pieces").then(pieces => pieces.json());
-  const response = await fetch('http://localhost:8081/pieces');
-  pieces = await response.json();
+	// Retrieve parts from the JSON file
+	// const pieces = await fetch("http://localhost:8081/pieces").then(pieces => pieces.json());
+	const response = await fetch('http://localhost:8081/pieces');
+	pieces = await response.json();
 
-  // Transforme the pieces into JSON
-  const piecesValue = JSON.stringify(pieces);
+	// Transforme the pieces into JSON
+	const piecesValue = JSON.stringify(pieces);
 
-  // Store the information in localStorage
-  window.localStorage.setItem('pieces', piecesValue);
+	// Store the information in localStorage
+	window.localStorage.setItem('pieces', piecesValue);
 } else {
-  pieces = JSON.parse(pieces);
+	pieces = JSON.parse(pieces);
 }
 
 // Call the function to add the listener on the reviews form
@@ -27,60 +27,60 @@ sendReviewsAddListener();
 
 // Function that generates the entire web page
 function generatePieces(pieces) {
-  for (let i = 0; i < pieces.length; i++) {
-    const article = pieces[i];
+	for (let i = 0; i < pieces.length; i++) {
+		const article = pieces[i];
 
-    // Retrieve the DOM element that will host the cards
-    const sectionSheets = document.querySelector('.sheets');
+		// Retrieve the DOM element that will host the cards
+		const sectionSheets = document.querySelector('.sheets');
 
-    // Create a tag dedicated to an automotive piece
-    const pieceElement = document.createElement('article');
+		// Create a tag dedicated to an automotive piece
+		const pieceElement = document.createElement('article');
 
-    // Create the img element
-    const imageElement = document.createElement('img');
-    // Access index i of the pieces list to configure the image source
-    imageElement.src = article.image;
+		// Create the img element
+		const imageElement = document.createElement('img');
+		// Access index i of the pieces list to configure the image source
+		imageElement.src = article.image;
 
-    const nameElement = document.createElement('h2');
-    nameElement.innerText = article.name;
+		const nameElement = document.createElement('h2');
+		nameElement.innerText = article.name;
 
-    const priceElement = document.createElement('p');
-    priceElement.innerText = `Prix: ${article.price} € (${
-      article.price < 35 ? '€' : '€€€'
-    })`;
+		const priceElement = document.createElement('p');
+		priceElement.innerText = `Prix: ${article.price} € (${
+			article.price < 35 ? '€' : '€€€'
+		})`;
 
-    const categoryElement = document.createElement('p');
-    categoryElement.innerText = article.category ?? '(aucune catégorie)';
+		const categoryElement = document.createElement('p');
+		categoryElement.innerText = article.category ?? '(aucune catégorie)';
 
-    const descriptionElement = document.createElement('p');
-    descriptionElement.innerText =
+		const descriptionElement = document.createElement('p');
+		descriptionElement.innerText =
       article.description ?? 'Pas de description pour le moment.';
 
-    const stockElement = document.createElement('p');
-    stockElement.innerText = article.availability
-      ? 'En stock'
-      : 'Rupture de stock';
+		const stockElement = document.createElement('p');
+		stockElement.innerText = article.availability
+			? 'En stock'
+			: 'Rupture de stock';
 
-    // Create reviews button to display reviews
-    const reviewsButton = document.createElement('button');
-    reviewsButton.dataset.id = article.id;
-    reviewsButton.textContent = 'Afficher les avis';
+		// Create reviews button to display reviews
+		const reviewsButton = document.createElement('button');
+		reviewsButton.dataset.id = article.id;
+		reviewsButton.textContent = 'Afficher les avis';
 
-    // Attach the article tag to the Sheets section
-    sectionSheets.appendChild(pieceElement);
-    // Attach the image to pieceElement (the article tag)
-    pieceElement.appendChild(imageElement);
-    pieceElement.appendChild(nameElement);
-    pieceElement.appendChild(priceElement);
-    pieceElement.appendChild(categoryElement);
-    pieceElement.appendChild(descriptionElement);
-    pieceElement.appendChild(stockElement);
-    // appended reviews button to its parent
-    pieceElement.appendChild(reviewsButton);
-  }
+		// Attach the article tag to the Sheets section
+		sectionSheets.appendChild(pieceElement);
+		// Attach the image to pieceElement (the article tag)
+		pieceElement.appendChild(imageElement);
+		pieceElement.appendChild(nameElement);
+		pieceElement.appendChild(priceElement);
+		pieceElement.appendChild(categoryElement);
+		pieceElement.appendChild(descriptionElement);
+		pieceElement.appendChild(stockElement);
+		// appended reviews button to its parent
+		pieceElement.appendChild(reviewsButton);
+	}
 
-  // Call reviewsAddListener function
-  reviewsAddListener();
+	// Call reviewsAddListener function
+	reviewsAddListener();
 }
 
 // Initial display of the page
@@ -88,68 +88,68 @@ generatePieces(pieces);
 
 // Retrieving reviews potentially stored in localStorage
 for (let i = 0; i < pieces.length; i++) {
-  const id = pieces[i].id;
-  const reviewsJSON = window.localStorage.getItem(`piece-reviews-${id}`);
-  const reviews = JSON.parse(reviewsJSON);
+	const id = pieces[i].id;
+	const reviewsJSON = window.localStorage.getItem(`piece-reviews-${id}`);
+	const reviews = JSON.parse(reviewsJSON);
 
-  if (reviews !== null) {
-    const pieceElement = document.querySelector(`article[data-id="${id}"]`);
+	if (reviews !== null) {
+		const pieceElement = document.querySelector(`article[data-id="${id}"]`);
 
-    // Add a check to ensure pieceElement is not null before calling displayReviews
-    if (pieceElement) {
-      displayReviews(pieceElement, reviews);
-    }
-  }
+		// Add a check to ensure pieceElement is not null before calling displayReviews
+		if (pieceElement) {
+			displayReviews(pieceElement, reviews);
+		}
+	}
 }
 
 // sort button management
 const sortButton = document.querySelector('.btn-sort');
 
 sortButton.addEventListener('click', function () {
-  const orderedPieces = Array.from(pieces);
-  orderedPieces.sort(function (a, b) {
-    return a.price - b.price;
-  });
-  // Clearing the screen and regenerating the page
-  document.querySelector('.sheets').innerHTML = '';
-  generatePieces(orderedPieces);
+	const orderedPieces = Array.from(pieces);
+	orderedPieces.sort(function (a, b) {
+		return a.price - b.price;
+	});
+	// Clearing the screen and regenerating the page
+	document.querySelector('.sheets').innerHTML = '';
+	generatePieces(orderedPieces);
 });
 
 // filter button management
 const filterButton = document.querySelector('.btn-filter');
 
 filterButton.addEventListener('click', function () {
-  const filteredPieces = pieces.filter(function (piece) {
-    return piece.price <= 35;
-  });
-  // Clearing the screen and regenerating the page with only the filtered pieces
-  document.querySelector('.sheets').innerHTML = '';
-  generatePieces(filteredPieces);
+	const filteredPieces = pieces.filter(function (piece) {
+		return piece.price <= 35;
+	});
+	// Clearing the screen and regenerating the page with only the filtered pieces
+	document.querySelector('.sheets').innerHTML = '';
+	generatePieces(filteredPieces);
 });
 
 // descending button management
 const descendingButton = document.querySelector('.btn-descending');
 
 descendingButton.addEventListener('click', function () {
-  const orderedPieces = Array.from(pieces);
-  orderedPieces.sort(function (a, b) {
-    return b.price - a.price;
-  });
-  // Clearing the screen and regenerating the page
-  document.querySelector('.sheets').innerHTML = '';
-  generatePieces(orderedPieces);
+	const orderedPieces = Array.from(pieces);
+	orderedPieces.sort(function (a, b) {
+		return b.price - a.price;
+	});
+	// Clearing the screen and regenerating the page
+	document.querySelector('.sheets').innerHTML = '';
+	generatePieces(orderedPieces);
 });
 
 // no description button management
 const noDescriptionButton = document.querySelector('.btn-no-desc');
 
 noDescriptionButton.addEventListener('click', function () {
-  const filteredPieces = pieces.filter(function (piece) {
-    return piece.description;
-  });
-  // Clearing the screen and regenerating the page with only the filtered pieces
-  document.querySelector('.sheets').innerHTML = '';
-  generatePieces(filteredPieces);
+	const filteredPieces = pieces.filter(function (piece) {
+		return piece.description;
+	});
+	// Clearing the screen and regenerating the page with only the filtered pieces
+	document.querySelector('.sheets').innerHTML = '';
+	generatePieces(filteredPieces);
 });
 
 // map the articles list
@@ -158,9 +158,9 @@ const articleNames = pieces.map(piece => piece.name);
 
 // delete non affordable articles
 for (let i = pieces.length - 1; i >= 0; i--) {
-  if (pieces[i].price > 35) {
-    articleNames.splice(i, 1);
-  }
+	if (pieces[i].price > 35) {
+		articleNames.splice(i, 1);
+	}
 }
 // console.log(articleNames);
 
@@ -172,16 +172,16 @@ const affordablesElements = document.createElement('ul');
 
 // add each name to the affordables list
 for (let i = 0; i < articleNames.length; i++) {
-  const articleNamesElement = document.createElement('li');
-  articleNamesElement.innerHTML = articleNames[i];
-  affordablesElements.appendChild(articleNamesElement);
+	const articleNamesElement = document.createElement('li');
+	articleNamesElement.innerHTML = articleNames[i];
+	affordablesElements.appendChild(articleNamesElement);
 }
 
 // add the header and then the list to the filter results block
 document
-  .querySelector('.affordables')
-  .appendChild(pElement)
-  .appendChild(affordablesElements);
+	.querySelector('.affordables')
+	.appendChild(pElement)
+	.appendChild(affordablesElements);
 
 // created two lists from pieces list
 const availableArticlesNames = pieces.map(piece => piece.name);
@@ -191,10 +191,10 @@ const availableArticlesPrices = pieces.map(piece => piece.price);
 
 // delete non available articles from the two lists above
 for (let i = pieces.length - 1; i >= 0; i--) {
-  if (!pieces[i].availability) {
-    availableArticlesNames.splice(i, 1);
-    availableArticlesPrices.splice(i, 1);
-  }
+	if (!pieces[i].availability) {
+		availableArticlesNames.splice(i, 1);
+		availableArticlesPrices.splice(i, 1);
+	}
 }
 // console.log(availableArticlesNames);
 // console.log(availableArticlesPrices);
@@ -204,34 +204,34 @@ const availablesElements = document.createElement('ul');
 
 // add each name and price to the availables list
 for (let i = 0; i < availableArticlesNames.length; i++) {
-  const availableArticleNamesElement = document.createElement('li');
-  availableArticleNamesElement.innerHTML = `${availableArticlesNames[i]} - ${availableArticlesPrices[i]} €`;
-  availablesElements.appendChild(availableArticleNamesElement);
+	const availableArticleNamesElement = document.createElement('li');
+	availableArticleNamesElement.innerHTML = `${availableArticlesNames[i]} - ${availableArticlesPrices[i]} €`;
+	availablesElements.appendChild(availableArticleNamesElement);
 }
 
 const pAvailableElement = document.createElement('p');
 pAvailableElement.innerText = 'Pièces disponibles:';
 document
-  .querySelector('.availables')
-  .appendChild(pAvailableElement)
-  .appendChild(availablesElements);
+	.querySelector('.availables')
+	.appendChild(pAvailableElement)
+	.appendChild(availablesElements);
 
 // retrieve input tag reference
 const maxPriceInput = document.querySelector('#max-price');
 
 maxPriceInput.addEventListener('input', function () {
-  const filteredPieces = pieces.filter(function (piece) {
-    return piece.price <= maxPriceInput.value;
-  });
-  // Clearing the screen and regenerating the page with only the filtered pieces
-  document.querySelector('.sheets').innerHTML = '';
-  generatePieces(filteredPieces);
+	const filteredPieces = pieces.filter(function (piece) {
+		return piece.price <= maxPriceInput.value;
+	});
+	// Clearing the screen and regenerating the page with only the filtered pieces
+	document.querySelector('.sheets').innerHTML = '';
+	generatePieces(filteredPieces);
 });
 
 // Adding the listener to update localStorage data
 const updateButton = document.querySelector('.btn-update');
 updateButton.addEventListener('click', function () {
-  window.localStorage.removeItem('pieces');
+	window.localStorage.removeItem('pieces');
 });
 
 // Call function to display chart graphic
